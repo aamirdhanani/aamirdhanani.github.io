f2720aae0a1e27fb3bf7467aca7cbfed37a82328
Aamir Dhanani <aamirdhanani@macbookpro.lan>
2025-11-14 12:26:55 -0500

    Switching to _site

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function(eleventyConfig) {
       dir: {
         input: "src",
         includes: "_includes",
-        output: "dist"
+        output: "_site"
       }
     };
   };

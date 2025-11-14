d6026a52fccc009ca3442cfef42096c268fd0a3c
Aamir Dhanani <aamirdhanani@macbookpro.lan>
2025-11-14 12:11:55 -0500

    Inital commit for new portfolio

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+    // Copy static files (images, CSS)
+    eleventyConfig.addPassthroughCopy("src/portfolio/photography");
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy({ "src/assets/js": "assets/js" });
+
+  
+    // Set input and output folders
+    return {
+      dir: {
+        input: "src",
+        includes: "_includes",
+        output: "dist"
+      }
+    };
+  };
